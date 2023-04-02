e639839683d2a60e77772634c595364a4187884d
Miftachul Huda <miftachulcc@gmail.com>
2023-04-02 10:28:55 +0700

    update set.sh

--- init.sh
+++ init.sh
@@ -9,3 +9,5 @@ fi
 
 wget https://github.com/miftachuda/Autovps/raw/master/sshws-only/sshws.sh && chmod +x sshws.sh && ./sshws.sh
 rm -f ./sshws.sh
+
+# wget https://github.com/miftachuda/Autovps/raw/master/xray/addtrojan.sh && chmod +x addtrojan.sh && ./addtrojan.sh && rm -f ./addtrojan.sh
